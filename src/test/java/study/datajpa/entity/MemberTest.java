0dfb39e2181bb399710695e74051b6701b152561
chaewon <cwlee4633@naver.com>
2024-02-26 14:47:39 +0900

    연관관계 확인

---
+++ src/test/java/study/datajpa/entity/MemberTest.java
@@ -0,0 +1,50 @@
+package study.datajpa.entity;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@SpringBootTest
+@Transactional
+@Rollback(value = false)
+class MemberTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void testEntity (){
+        Team teamA = new Team("teamA");
+        Team teamB = new Team("teamB");
+        em.persist(teamA);
+        em.persist(teamB);
+
+        Member member1 = new Member("member1", 10, teamA);
+        Member member2 = new Member("member2", 11, teamA);
+        Member member3 = new Member("member3", 12, teamB);
+        Member member4 = new Member("member4", 13, teamB);
+        em.persist(member1);
+        em.persist(member2);
+        em.persist(member3);
+        em.persist(member4);
+
+        //초기화
+        em.flush();  //insert 쿼리 날리기
+        em.clear();  //캐시 날리기
+
+        //확인
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+
+        for (Member member : members) {
+            System.out.println("member = " + member);
+            System.out.println("-> member.team = " + member.getTeam());
+        }
+    }
+
+}
